ec1fb9088d5162a21b7873904f2ff0ff35f4c7ea
Karol Moroz <karol@moroz.dev>
2025-02-22 16:19:31 +0800

    Code from the tutorial

--- main.go
+++ main.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/debug"
+)
+
+type handler struct{}
+
+func (h *handler) Execute(args []string, r <-chan svc.ChangeRequest, status chan<- svc.Status) (bool, uint32) {
+	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown | svc.AcceptPauseAndContinue
+	tick := time.NewTicker(10 * time.Second)
+	// tick := time.Tick(10 * time.Second)
+	status <- svc.Status{State: svc.StartPending}
+	status <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+
+loop:
+	for {
+		select {
+		case <-tick.C:
+			log.Print("Tick handled!")
+		case c := <-r:
+			switch c.Cmd {
+			case svc.Interrogate:
+				status <- c.CurrentStatus
+			case svc.Stop, svc.Shutdown:
+				log.Print("Shutting down...")
+				break loop
+			case svc.Pause:
+				status <- svc.Status{State: svc.Paused, Accepts: cmdsAccepted}
+				tick.Stop()
+			case svc.Continue:
+				status <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+				tick = time.NewTicker(10 * time.Second)
+			default:
+				log.Printf("Unexpected service control request #%d", c)
+			}
+		}
+	}
+
+	status <- svc.Status{State: svc.StopPending}
+	return false, 1
+}
+
+func runService(name string, isDebug bool) {
+	if isDebug {
+		err := debug.Run(name, &handler{})
+		if err != nil {
+			log.Fatalf("Error running service %s in debug mode: %s", name, err)
+		}
+	} else {
+		err := svc.Run(name, &handler{})
+		if err != nil {
+			log.Fatalf("Error running service %s in Service Control mode: %s", name, err)
+		}
+	}
+}
+
+var DEBUG = os.Getenv("DEBUG") != ""
 
 func main() {
-	fmt.Println("Hello, world!")
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logFile := path.Join(pwd, `log/debug.log`)
+
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0o666)
+	if err != nil {
+		log.Fatalf("error opening file: %s", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	runService("moroz-testservice", DEBUG)
 }
